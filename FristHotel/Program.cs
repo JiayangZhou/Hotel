c6068c9ecfc8035672718edf54691d1e23eb2177
JiayangZhou <a17zhoji@student.his.se>
2019-01-27 21:21:37 +0100

    Upload files

---
+++ FristHotel/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FristHotel
+{
+    class Program
+    {
+        public static string notHereFilePath = @"C:\Users\dengd\Ctest\notHereGuests.txt";
+        static void Main(string[] args)
+        {
+
+            //WelcomeGuests();
+            //NotHereGuests();
+            
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+
+
+
+        public static void NotHereGuests()
+        {
+            List<Guest> notHereGuests = new List<Guest>();
+            List<string> lines = File.ReadAllLines(notHereFilePath).ToList();
+            foreach (string line in lines)
+            {
+                string[] details = line.Split(' ');
+
+                Guest gettingInf = new Guest();
+
+                gettingInf.FirstName = details[0];
+                gettingInf.LastName = details[1];
+                gettingInf.RoomNum = details[2];
+
+                notHereGuests.Add(gettingInf);
+
+            }
+            foreach (Guest guest in notHereGuests)
+            {
+                Console.WriteLine($"{guest.FirstName} is not here");
+            }
+
+            //Add another two people to notHereGuests
+            Guest bob = new Guest() { FirstName = "Bob", LastName = "Smith", RoomNum = "101" };
+            notHereGuests.Add(bob);
+            notHereGuests.Add(new Guest { FirstName = "Robert", LastName = "Smith", RoomNum = "104" });        
+            
+            Console.WriteLine("\nAfter two other people gone missing");
+            foreach (Guest guest in notHereGuests)
+            {
+                Console.WriteLine($"{guest.FirstName} is not here");
+            }
+
+            //Rewrite notHereFile
+            lines.Clear();
+            foreach (var guest in notHereGuests)
+            {
+                lines.Add($"{guest.FirstName} {guest.LastName} {guest.RoomNum}");
+            }
+            File.WriteAllLines(notHereFilePath, lines);
+        }
+
+        public static void WelcomeGuests()
+        {
+            List<Guest> guests = new List<Guest>();
+            Guest fabian = new Guest() { FirstName = "Fabian", LastName = "Wimmer" };
+            guests.Add(fabian);
+            guests.Add(new Guest { FirstName = "Jiayang", LastName = "Zhou" });
+            guests.Add(new Guest { FirstName = "Harry", LastName = "Potter" });
+            guests.Add(new Guest { FirstName = "Donald", LastName = "Trump" });
+
+
+            foreach (var guest in guests)
+            {
+                Console.WriteLine($"Hej, {guest.FirstName} {guest.LastName}. Welcome to our hotel!");
+            }
+
+            /*          
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            lines.Add("line1, line2, line3");
+            File.WriteAllLines(filePath, lines);
+            */
+        }
+    }
+}
---
+++ SecondHotel/Construction.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHotel
+{
+    class Construction
+    {
+        public void Blueprint()
+        {
+            Rectangle Rect = new Rectangle();
+            int area;
+
+            Rect.setWidth(5);
+            Rect.setHeight(7);
+            //Rect.width = 5;
+            //Rect.height = 7;
+            area = Rect.getArea();
+
+            // Print the area of the object.
+            Console.WriteLine("Total area: {0}", Rect.getArea());
+            Console.WriteLine("Total paint cost: ${0}", Rect.getCost(area));
+            Console.ReadKey();
+        }
+
+        public class Shape
+        {
+            public void setWidth(int w)
+            {
+                width = w;
+            }
+            public void setHeight(int h)
+            {
+                height = h;
+            }
+            protected int width;
+            protected int height;
+            //public int width { get; set; }
+
+            //public int height { get; set; }
+        }
+
+        //Base class PaintCost
+        //public interface PaintCost
+        //{
+        //    int getCost(int area);
+        //}
+        public class Rectangle : Shape/*, PaintCost*/
+        {
+            public int getArea()
+            {
+                return (width * height);
+            }
+            public int getCost(int area)
+            {
+                return area * 70;
+            }
+        }
+    }
+}
